70f3621541b46a980dc1222e83d9269d1ee2c8e5
Scott Davis <scottdavisxx@gmail.com>
2022-03-12 20:26:33 -0500

    create and call Pet element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+const Pet = () => {
+    return React.createElement("div",{}, [
+        React.createElement("h2", {}, "Murphy"),
+        React.createElement("h3", {}, "Dog"),
+        React.createElement("h3", {}, "Goldendoodle"),
+
+    ])
+}
+
 const App = () => {
     return React.createElement (
         "div",
         {},
-        React.createElement("h1",{}, "Adopt Me!")
+        [
+            React.createElement("h1",{}, "Adopt Me!"),
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet),
+        ]
     )
 }
 
