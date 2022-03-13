4c4d738fb13558f471fbbfe8b476f244210daaca
Scott Davis <scottdavisxx@gmail.com>
2022-03-12 22:33:15 -0500

    run prettier

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Murphy" animal="Dog" breed="Goldendoodle"/>
-      <Pet name="Montana" animal="Dog" breed="Pomeranian"/>
-      <Pet name="Toby" animal="Dog" breed="Goldendoodle"/>
+      <Pet name="Murphy" animal="Dog" breed="Goldendoodle" />
+      <Pet name="Montana" animal="Dog" breed="Pomeranian" />
+      <Pet name="Toby" animal="Dog" breed="Goldendoodle" />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -15,7 +15,7 @@ const Pet = (props) => {
       <h3>{props.animal}</h3>
       <h3>{props.breed}</h3>
     </div>
-  )
-}
+  );
+};
 
 export default Pet;
