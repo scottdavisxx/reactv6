6824e88d74ba2c37936a2ac2857e7dd783936026
Scott Davis <scottdavisxx@gmail.com>
2022-03-12 20:37:01 -0500

    create and pass props to all pets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div",{}, [
-        React.createElement("h2", {}, "Murphy"),
-        React.createElement("h3", {}, "Dog"),
-        React.createElement("h3", {}, "Goldendoodle"),
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed),
 
     ])
 }
@@ -12,10 +12,10 @@ const App = () => {
         "div",
         {},
         [
-            React.createElement("h1",{}, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement("h1",{id: "my-brand"}, "Adopt Me!"),
+            React.createElement(Pet, {name: "Murphy", animal: "Dog", breed: "Golendoodle"}),
+            React.createElement(Pet, {name: "Montana", animal: "Dog", breed: "Pomeranian"}),
+            React.createElement(Pet, {name: "Toby", animal: "Dog", breed: "Golendoodle"}),
         ]
     )
 }
