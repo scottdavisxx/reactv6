c7865d0aca530099f65a09ab7ed95418a24c71a8
Scott Davis <scottdavisxx@gmail.com>
2022-03-19 12:40:13 -0400

    add automatic redirect on ErrorBoundary

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,21 +1,28 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError() {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
     // Log this to Sentry, Azure Monitor, NewRelic, TrackJS
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({redirect: true }), 5000)
   }
+
   render() {
+    if(this.state.redirect){
+      return(
+        <Redirect to="/" />
+      )
+    }
     if (this.state.hasError) {
       return (
         <h2>
           This listing has an error.{" "}
-          <Link to="/">Click here to go back to the Homepage.</Link>
+          <Link to="/">Click here to go back to the Homepage. Or you will be automatically redirected.</Link>
         </h2>
       );
     }
