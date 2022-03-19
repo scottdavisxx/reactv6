8c336b3904cd7e692e37346fd33ea79f3fb4784c
Scott Davis <scottdavisxx@gmail.com>
2022-03-19 12:21:34 -0400

    Create error boundary component

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    // Log this to Sentry, Azure Monitor, NewRelic, TrackJS
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h1>
+          This listing has an error.{" "}
+          <Link to="/">Click here to go back to the Homepage.</Link>
+        </h1>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
